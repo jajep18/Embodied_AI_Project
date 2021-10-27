661d6044a9ed115e8d64bd9a96eec1a6a18beba5
jajep18 <jajep18@student.sdu.dk>
2021-10-27 16:47:46 +0200

    Updated the grabber test script

--- grabberTest.py
+++ grabberTest.py
@@ -3,30 +3,28 @@ import ev3dev.ev3 as ev3
 from time import sleep
 import signal
 
-sColorTouch = ev3.ColorSensor('in1') # Touch color sensor (claw)
+#sColorTouch = ev3.ColorSensor('in1') # Touch color sensor (claw)
 btn     = ev3.Button()           # Control block buttons
 mClaw  = ev3.LargeMotor('outB') # Claw motor
 
-sColorTouch.mode='COL-COLOR'
+#sColorTouch.mode='COL-COLOR'
 grap = -85
 release = -grap
 
-
-
 mClaw.run_direct()
+
+
 while(True):
-    if sColorTouch.value() == 5:
+    
 
-        mClaw.run_to_rel_pos(position_sp=grap, speed_sp=100, stop_action="coast")
+    mClaw.run_to_rel_pos(position_sp=grap, speed_sp=200, stop_action="coast")
 
-        sleep(2)   # Give the motor time to move
-
-        mClaw.run_to_rel_pos(position_sp=release, speed_sp=100, stop_action="hold")
-
-        sleep(2)
+    sleep(2)   # Give the motor time to move
 
     if btn.any():
         ev3.Sound.beep().wait()
+        mClaw.run_to_rel_pos(position_sp=release, speed_sp=100, stop_action="hold")
+        sleep(2)
         mClaw.duty_cycle_sp = 0
         exit()
 
