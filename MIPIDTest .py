5a49e6d4fe112afb285e1d3ac018bc10fd61dcc3
jajep18 <jajep18@student.sdu.dk>
2021-11-03 12:46:58 +0100

    Updated PID controller and grapper test file

--- MIPIDTest .py
+++ MIPIDTest .py
@@ -36,36 +36,53 @@ TARGET = 0 #(BLACK + WHITE ) / 2  # = 80 + 10 / 2 = 45
 
 # - - - - - - - - - - PID CONTROL - - - - - - - - - -
 # GAINS
-Kp = 0.42
-Ki = 0.0008
-Kd = 0.001
+Kp = 6.6161  #0.42
+Ki = 53.112  #0.0008
+Kd = 0.20553 #0.001
 # Components
 integral   = 0
 derivative = 0
 last_error = 0
 error      = 0
+error_sum  = 0
 # - - - - - - - - - - CONTROL LOOP - - - - - - - - - -
 while True:
     # - - - LOAD SENSOR VALUES - - -
     sColorL_val= sColorL.value()
     sColorR_val= sColorR.value()
-
+    print("Left sensor: " + str(sColorL_val) + " Right sensor " + str(sColorR_val))
     # PID Line Follower
 
-    com_input = (sColorL_val - sColorR_val) / 2
-    print("Combined input =" + str(com_input))
+    com_input = (sColorL_val - sColorR_val) / 2 # If negative go left. If Possitive go right
+    #print("Combined input =" + str(com_input))
     error = com_input - TARGET
     integral = integral + error
-    derivative = error - last_error
-    last_error = error
+    derivative = error - error_sum #Current error - last 10 error values
+    
+    #Update error array
+    last_error[9] = last_error[8]
+    last_error[8] = last_error[7]
+    last_error[7] = last_error[6]
+    last_error[6] = last_error[5]
+    last_error[5] = last_error[4]
+    last_error[4] = last_error[3]
+    last_error[3] = last_error[2]
+    last_error[2] = last_error[1]
+    last_error[1] = last_error[0]   
+    last_error[0] = error           #Current error
+
+    error_sum = 0
+    for i in range( 0 , len(last_error)-1 ):
+        error_sum -= last_error[i] - last_error[i+1]
+    
 
     turn_val = Kp * error + Ki * integral + Kp * derivative
-    print("Turn value: " +str(turn_val))
+    #print("Turn value: " +str(turn_val))
     mRight_val = DRIVE_SPEED + int(turn_val)
     mLeft_val = DRIVE_SPEED - int(turn_val)
-    print("mRight: " + str(mRight_val) + "  mLeft: " + str(mLeft_val))
-    mRight.duty_cycle_sp = mRight_val
-    mLeft.duty_cycle_sp  = mLeft_val
+    #print("mRight: " + str(mRight_val) + "  mLeft: " + str(mLeft_val))
+    #mRight.duty_cycle_sp = mRight_val
+    #mLeft.duty_cycle_sp  = mLeft_val
 
 
     sleep(0.01)
--- grabberTest.py
+++ grabberTest.py
@@ -3,29 +3,27 @@ import ev3dev.ev3 as ev3
 from time import sleep
 import signal
 
-#sColorTouch = ev3.ColorSensor('in1') # Touch color sensor (claw)
+sColorTouch = ev3.LightSensor('in2') # Touch color sensor (claw)
 btn     = ev3.Button()           # Control block buttons
-mClaw  = ev3.MediumMotor('outB') # Claw motor
+mClaw  = ev3.LargeMotor('outB') # Claw motor
 
 #sColorTouch.mode='COL-COLOR'
-grap = -285
+grap = 85
 release = -grap
 
 mClaw.run_direct()
 
 
 while(True):
-    
+    print(str(sColorTouch.value()))
+    if sColorTouch.value() >= 500:
+        mClaw.run_to_rel_pos(position_sp=grap, speed_sp=200, stop_action="hold")
 
-    mClaw.run_to_rel_pos(position_sp=grap, speed_sp=200, stop_action="hold")
-
-    sleep(2)   # Give the motor time to move
-    mClaw.run_to_rel_pos(position_sp=release, speed_sp=100, stop_action="coast")
-    sleep(2)
+        sleep(2)   # Give the motor time to move
+        mClaw.run_to_rel_pos(position_sp=release, speed_sp=100, stop_action="coast")
+        sleep(2)
 
     if btn.any():
         ev3.Sound.beep().wait()
         mClaw.duty_cycle_sp = 0
         exit()
-
-
