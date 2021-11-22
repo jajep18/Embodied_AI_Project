a564f6b22fcdefaa2130e6d38c7b2321d5ced959
ELindby <erik.lindby@gmail.com>
2021-11-22 17:03:04 +0100

    Updated motor connections

--- StopMotor.py
+++ StopMotor.py
@@ -4,17 +4,18 @@ from time import sleep
 import signal
 
 mA = ev3.LargeMotor('outA')
-#mB = ev3.MediumMotor('outB')
-#mC = ev3.MediumMotor('outC')
+#mB = ev3.LargeMotor('outB')
+mC = ev3.LargeMotor('outC')
 mD = ev3.LargeMotor('outD')
 
 mA.run_direct()
 #mB.run_direct()
-#mC.run_direct()
+mC.run_direct()
 mD.run_direct()
 
 mA.duty_cycle_sp = 0
 #mB.duty_cycle_sp = 0
-#mC.duty_cycle_sp = 0
+mC.duty_cycle_sp = 0
 mD.duty_cycle_sp = 0
+
 exit()
