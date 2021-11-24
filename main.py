69516e26e00b26d3b357c90bdf6e19a33aa08132
jajep18 <jajep18@student.sdu.dk>
2021-11-24 11:56:51 +0100

    Update_24_11

--- main.py
+++ main.py
@@ -39,26 +39,16 @@ TARGET = 0 #(BLACK + WHITE ) / 2  # = 80 + 10 / 2 = 45
 
 # - - - - - - - - - - PID CONTROL - - - - - - - - - -
 # GAINS
-<<<<<<< HEAD
 Kp = 10.2 #6.6161  #0.42
 Ki = 1.3#1.5#3#0.53112  #0.0008
 Kd = 0.41#0.20553 #0.001
 BACKWARDS_GAIN = 2.5
-=======
-Kp = 6.6161  #0.42
-Ki = 2#0.53112  #0.0008
-Kd = 0.20553 #0.001
->>>>>>> 4b59d8457e855cd3422f529fcb3abbce6d28f6d0
 # Components
 integral   = 0
 derivative = 0
 last_error = 0
 error      = 0
-<<<<<<< HEAD
 integral_clamp = 5
-=======
-integral_clamp = 20
->>>>>>> 4b59d8457e855cd3422f529fcb3abbce6d28f6d0
 
 # - - - - - - - - - - FUNCTIONS - - - - - - - - - -
 def setMotorSP(motorHandle, sp_input): 
@@ -81,18 +71,11 @@ while True:
     last_error = error # Save last error for derivative
 
     # - - - - - - - - - - PID Line Follower - - - - - - - - - -
-<<<<<<< HEAD
     # INPUT / ERROR [-5;5]
     com_input = (sColorL_val - sColorR_val) / 20 #/2# If negative go left. If Positive go right
     #print("Combined input =" + str(com_input))
     error = com_input - TARGET
     integral = integral * (4/5) + error
-=======
-    com_input = (sColorL_val - sColorR_val) / 10 #/2# If negative go left. If Positive go right
-    #print("Combined input =" + str(com_input))
-    error = com_input - TARGET
-    integral = integral + error
->>>>>>> 4b59d8457e855cd3422f529fcb3abbce6d28f6d0
     derivative = error - last_error #Current error - last 10 error values
     #print("Left sensor: " + str(sColorL_val) + " Right sensor " + str(sColorR_val) + " com_input: " + str(com_input))
     
@@ -108,7 +91,6 @@ while True:
     #turn_val = Kp * error + Ki * integral + Kd * derivative
     turn_val = Kp * error + Ki * integral + Kd * derivative
     #print("PID-P(error): " + str(error) + " I: " + str(integral) + " D: " + str(derivative))
-<<<<<<< HEAD
 
     #print("Turn value: " + str(turn_val))
     DRIVE_SPEED_corr = DRIVE_SPEED #* (abs(int(integral))-integral_clamp)/(-20)
@@ -121,15 +103,6 @@ while True:
     else:
         mRight_val = DRIVE_SPEED_corr + int(turn_val) * BACKWARDS_GAIN
         mLeft_val  = DRIVE_SPEED_corr - int(turn_val)
-=======
- 
-
-
-    #print("Turn value: " + str(turn_val))
-    DRIVE_SPEED_corr = DRIVE_SPEED * (abs(int(integral))-integral_clamp)/(-20)
-    mRight_val =  DRIVE_SPEED_corr + int(turn_val)
-    mLeft_val = DRIVE_SPEED_corr - int(turn_val)
->>>>>>> 4b59d8457e855cd3422f529fcb3abbce6d28f6d0
     #print("mRight: " + str(mRight_val) + "  mLeft: " + str(mLeft_val))
     #mRight.duty_cycle_sp = mRight_val
     #mLeft.duty_cycle_sp  = mLeft_val
@@ -154,8 +127,4 @@ while True:
         sleep(2)   # Give the motor time to move
         mClaw.run_to_rel_pos(position_sp=RELEASE_ANGLE, speed_sp=100, stop_action="coast")
         sleep(2)
-<<<<<<< HEAD
         #TODO: Add turning around - Dont let go of can again
-=======
-        #TODO: Add turning around - Dont let go of can again
->>>>>>> 4b59d8457e855cd3422f529fcb3abbce6d28f6d0
