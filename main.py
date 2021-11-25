87a44a14245c6d34c61445d87cce0cb3d300c4ab
jajep18 <jajep18@student.sdu.dk>
2021-11-25 15:57:21 +0100

    Fixed hill climbing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from ev3dev.core import GyroSensor
 import ev3dev.ev3 as ev3
 from time import sleep
 import signal
@@ -8,6 +9,8 @@ import array
 mRight = ev3.LargeMotor('outD')  # Right wheel motor
 mLeft  = ev3.LargeMotor('outA')  # Left wheel motor
 mClaw  = ev3.LargeMotor('outC') # Claw motor
+
+
 
 mRight.run_direct()
 mLeft.run_direct()
@@ -21,35 +24,42 @@ RELEASE_ANGLE = -GRAB_ANGLE
 
 # - - - - - - - - - - SETUP SENSORS - - - - - - - - - -
 btn     = ev3.Button()           # Control block buttons
-#sGyro   = ev3.GyroSensor('in1')  # Gyro sensor
+sGyro   = ev3.GyroSensor('in2')  # Gyro sensor
 sColorR = ev3.ColorSensor('in4') # Right color sensor
 sColorL = ev3.ColorSensor('in1') # Left color sensor
 sLightC = ev3.LightSensor('in3') # Light sensor for claw
 
 
+
 # Reflected light, value between 0 and 100, ~80 white, ~5 black
 sColorR.mode='COL-REFLECT'
 sColorL.mode='COL-REFLECT'
+sGyro.mode='GYRO-RATE'
+
+sleep(0.5)
 
 WHITE = 80
 BLACK = 10
 TARGET = 0 #(BLACK + WHITE ) / 2  # = 80 + 10 / 2 = 45
+
+gyro_offset = sGyro.value()
 
 # - - - - - - - - - - PID CONTROL - - - - - - - - - -
 # GAINS
 d_T = 0.0141 #[ms]
 K_c = 50
 P_c = 0.75 
-Kp =  30  #0.6 * K_c                  #12.2 #6.6161  #0.42
+Kp =  25#30  #0.6 * K_c                  #12.2 #6.6161  #0.42
 Ki =  2#1.128#1.128  #2 * Kp * d_T / P_c         #2.5#1.3#1.5#3#0.53112  #0.0008
-Kd =  10#199.47  #Kp * P_c / (8*d_T)         #4.84
-BACKWARDS_GAIN = 1.2             # 2.2#2.5
+Kd =  5#10#199.47  #Kp * P_c / (8*d_T)         #4.84
+BACKWARDS_GAIN = 1# 1.2             # 2.2#2.5
 # Components
 integral   = 0
 derivative = 0
 last_error = 0
 error      = 0
 integral_clamp = 10
+error_max = 1.8
 
 # - - - - - - - - - - FUNCTIONS - - - - - - - - - -
 def setMotorSP(motorHandle, sp_input): 
@@ -66,6 +76,8 @@ def setMotorSP(motorHandle, sp_input):
 
 ev3.Sound.beep().wait()
 while True:
+    gyro_val = sGyro.value()-gyro_offset
+    print(str(gyro_val))
     
     # - - - LOAD SENSOR VALUES - - -
     sColorL_val= sColorL.value()
@@ -75,11 +87,13 @@ while True:
     # - - - - - - - - - - PID Line Follower - - - - - - - - - -
     # INPUT / ERROR = [-5;5]
     com_input = (sColorR_val - sColorL_val) / 20 #/2# If negative go left. If Positive go right
-    #print("Error L: " + str(sColorL_val )+ "Error R: " + str(sColorR_val))
     error = com_input - TARGET
+    
     integral = integral * (3/4) + error
     derivative = error - last_error #Current error - last 10 error values
-    
+    # if abs(error) > abs(error_max):
+    #     error_max = error
+    #print("Error: " + str(error) +" sL: " + str(sColorL_val )+ " sR: " + str(sColorR_val) + " Emax: " + str(error_max))
     
     ##################### Integrator clamping to remove windup #########################
     if(abs(integral) > integral_clamp and integral > 0):
@@ -91,9 +105,11 @@ while True:
 
     turn_val = Kp * error + Ki * integral + Kd * derivative
 
+    if abs(error) > error_max:
+        error = error_max * error/abs(error)
+
     
-    
-    DRIVE_SPEED_corr = DRIVE_SPEED * (1 - abs(error)/3)
+    DRIVE_SPEED_corr = DRIVE_SPEED * (1 - abs(error)/error_max)
     
      #* (abs(int(integral))-integral_clamp)/(-20)
     #mRight_val =  DRIVE_SPEED_corr + int(turn_val)
@@ -107,8 +123,8 @@ while True:
         mLeft_val  = DRIVE_SPEED_corr - int(turn_val)
     #print("mRight: " + str(mRight_val) + "  mLeft: " + str(mLeft_val))
 
-    setMotorSP(mRight, mRight_val)
-    setMotorSP(mLeft, mLeft_val)
+    #setMotorSP(mRight, mRight_val)
+    #setMotorSP(mLeft, mLeft_val)
 
     
     #sleep(0.001)
